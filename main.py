f4fabce6c4629bb4eff323e01c87051378fb5ce1
Piotr Wójcicki <pwojcicki1990@gmail.com>
2018-03-13 19:26:23 +0100

    removed comments and inserted clear

--- main.py
+++ main.py
@@ -2,35 +2,38 @@ import os
 import numpy as np
 import cv2, time
 import zipfile
+from datetime import datetime
 
 pwd = os.getcwd()
 files = []
+date = datetime.now().isoformat(timespec="minutes")
+date = date[0:-3] + date[-2::]
 
 def zip(files_table):
-    zf = zipfile.ZipFile('zipfile_write.zip', mode='a')
+    zip_name = str(date) + ".zip"
+    print(zip_name)
+    zf = zipfile.ZipFile(zip_name, mode='a')
     for file in files_table:
         zf.write(file)
     zf.close()
 
+"""def clear(files_table):
+    for file in files_table:
+        os.remove(file)
+    os.remove()"""
+
+
+
+
 def cam(fr):
     import cv2
     camera_port = 0
-
-    # Number of frames to throw away while the camera adjusts to light levels
     ramp_frames = fr
-
-    # Now we can initialize the camera capture object with the cv2.VideoCapture class.
-    # All it needs is the index to a camera port.
     camera = cv2.VideoCapture(camera_port)
-
-    # Captures a single image from the camera and returns it in PIL format
     def get_image():
-        # read is the easiest way to get a full image out of a VideoCapture object.
         retval, im = camera.read()
         return im
 
-    # Ramp the camera - these frames will be discarded and are only used to allow v4l2
-    # to adjust light levels, if necessary
     for i in range(ramp_frames):
         temp = get_image()
     camera_capture = get_image()
@@ -39,6 +42,6 @@ def cam(fr):
     cv2.imwrite(file, camera_capture)
     del (camera)
 cam(0)
-cam(15)
-cam(30)
+cam(1)
+cam(2)
 zip(files)
